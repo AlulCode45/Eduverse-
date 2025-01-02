47f0cecbc8eb2b56a07c2d5b1388a48c7fdb521f
AlulCode45 <alulgans70@gmail.com>
2025-01-02 17:23:52 +0700

    feat: register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:starter_pack/domains/utils/routers.dart';
 import 'package:starter_pack/screens/home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final Routers _routers = Routers();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Eduverse - Aplikasi Belajar No #1 di indonesia',
-      home: Home(),
       theme: ThemeData(
         fontFamily: 'Poppins',
         useMaterial3: true
       ),
+      routes: _routers.routers,
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/screens/auth/loginPage.dart
+++ lib/screens/auth/loginPage.dart
@@ -1,10 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
 
 class Loginpage extends StatelessWidget {
   const Loginpage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: HugeIcon(
+              icon: HugeIcons.strokeRoundedArrowLeft01, color: Colors.black),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                // alignment: Alignment.center,
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account?',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff525F7F),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'Recomece de onde parou',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    SizedBox(
+                      height: 60,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff686BFF),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'Login',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Divider(),
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Are you new here?',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff525F7F),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'Start Learning today',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    SizedBox(
+                      height: 60,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          // backgroundColor: Color(0xff686BFF),
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Color(0xff686BFF),
+                              style: BorderStyle.solid,
+                              width: 2
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'Create an account',
+                          style: TextStyle(fontSize: 20, color: Color(0xff686BFF)),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
