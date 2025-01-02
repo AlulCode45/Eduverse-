b12d1870c35b0986d6f753ad322d590f898e2f3c
AlulCode45 <alulgans70@gmail.com>
2025-01-02 17:26:27 +0700

    feat: routers.dart

--- lib/domains/utils/routers.dart
+++ lib/domains/utils/routers.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:starter_pack/screens/auth/loginForm.dart';
 import 'package:starter_pack/screens/auth/loginPage.dart';
+import 'package:starter_pack/screens/auth/registerForm.dart';
+import 'package:starter_pack/screens/auth/registerPage.dart';
 import 'package:starter_pack/screens/home.dart';
 
 class Routers {
   Map<String, WidgetBuilder> routers = {
-      '/': (BuildContext context) => Loginpage(),
-      '/auth': (BuildContext context) => Home()
-    };
+    '/': (BuildContext context) => Loginpage(),
+    '/login': (BuildContext context) => Loginform(),
+    '/register': (BuildContext context) => Registerpage(),
+    '/register/form': (BuildContext context) => Registerform(),
+  };
 }
