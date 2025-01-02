1477889471ee7ea6ccb183d84f036360f2e1ac66
AlulCode45 <alulgans70@gmail.com>
2025-01-02 19:56:57 +0700

    feat: add gesture detector

--- lib/components/signUpOption.dart
+++ lib/components/signUpOption.dart
@@ -4,32 +4,36 @@ class Signupoption extends StatelessWidget {
   final IconData icon;
   final String text;
   final Color? background;
+  final void Function() onTap;
 
   Signupoption(
-      {super.key, required this.icon, required this.text, this.background});
+      {super.key, required this.icon, required this.text, this.background, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 25,vertical: 35),
-      margin: EdgeInsets.only(top: 16),
-      decoration: BoxDecoration(
-        color: background ?? Colors.blue,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        spacing: 20,
-        children: [
-          Icon(
-            icon,
-            color: Colors.white,
-            size: 30,
-          ),
-          Text(
-            text,
-            style: TextStyle(color: Colors.white, fontSize: 21),
-          ),
-        ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 25,vertical: 35),
+        margin: EdgeInsets.only(top: 16),
+        decoration: BoxDecoration(
+          color: background ?? Colors.blue,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          spacing: 20,
+          children: [
+            Icon(
+              icon,
+              color: Colors.white,
+              size: 30,
+            ),
+            Text(
+              text,
+              style: TextStyle(color: Colors.white, fontSize: 21),
+            ),
+          ],
+        ),
       ),
     );
   }
